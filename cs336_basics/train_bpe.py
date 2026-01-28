0f8454d7e6d75b6ab0fcf5b010f474bafdd56476
Zhu Lingkai <zhulingkai123@gmail.com>
2026-01-28 08:56:21 +0200

    optimize train bpe

--- cs336_basics/train_bpe.py
+++ cs336_basics/train_bpe.py
@@ -1,10 +1,12 @@
 from typing import Tuple
 import regex as re
-from cs336_basics.pretokenization_example import find_chunk_boundaries
 from collections import Counter
 import multiprocessing as mp
 import time
+import json
 
+from cs336_basics.pretokenization_example import find_chunk_boundaries
+from tests.common import gpt2_bytes_to_unicode
 def process_chunk(boundary1, boundary2, input_path, special_tokens, PAT):
     frequency_table = Counter()
     with open(input_path, "rb") as f:
@@ -13,8 +15,6 @@ def process_chunk(boundary1, boundary2, input_path, special_tokens, PAT):
         # remove special tokens from chunk
         splits = re.split("|".join(re.escape(tok) for tok in special_tokens), chunk)
         for split in splits:
-            if split.strip() == "":
-                continue
             pretokenized_data = re.findall(PAT, split)
             for token in pretokenized_data:
                 token_bytes = token.encode('utf-8')
@@ -29,7 +29,7 @@ def train_bpe(input_path: str, vocab_size: int, special_tokens: list[str]) -> Tu
     merges = []
     PAT = r"""'(?:[sdmt]|ll|ve|re)| ?\p{L}+| ?\p{N}+| ?[^\s\p{L}\p{N}]+|\s+(?!\S)|\s+"""
     with open(input_path, "rb") as f:
-        num_processes = 8
+        num_processes = 16
         boundaries = find_chunk_boundaries(f, num_processes, b"<|endoftext|>")
         # parallel implementation
         time1 = time.time()
@@ -51,6 +51,7 @@ def train_bpe(input_path: str, vocab_size: int, special_tokens: list[str]) -> Tu
     time2 = time.time()
     print(f"Time taken for initial pair frequency calculation: {time2 - time1} seconds")
     time1 = time.time()
+
     while len(vocab) < vocab_size:
         # take the max lexicographical pair
         best_pair = max(pair_frequencies.items(), key=lambda x: (x[1], x[0]))[0]
@@ -66,28 +67,27 @@ def train_bpe(input_path: str, vocab_size: int, special_tokens: list[str]) -> Tu
             merged = []
             a, b = best_pair
             i = 0
-            while i < len(token_tuple):
-                if i < len(token_tuple) - 1 and token_tuple[i] == a and token_tuple[i + 1] == b:
-                    merged.append(a + b)
-                    if i - 1 >= 0:
-                        new_pair = (token_tuple[i - 1], a + b)
-                        pair_frequencies[new_pair] = pair_frequencies.get(new_pair, 0) + freq
-                        pair_frequencies[(token_tuple[i - 1], a)] = pair_frequencies.get((token_tuple[i - 1], a), 0) - freq
-                    if i + 2 < len(token_tuple):
-                        new_pair = (a + b, token_tuple[i + 2])
-                        pair_frequencies[new_pair] = pair_frequencies.get(new_pair, 0) + freq
-                        pair_frequencies[(b, token_tuple[i + 2])] = pair_frequencies.get((b, token_tuple[i + 2]), 0) - freq
+            if a in token_tuple and b in token_tuple:
+                while i < len(token_tuple):
+                    if i < len(token_tuple) - 1 and token_tuple[i] == a and token_tuple[i + 1] == b:
+                        merged.append(a + b)
+                        if i - 1 >= 0:
+                            new_pair = (token_tuple[i - 1], a + b)
+                            pair_frequencies[new_pair] = pair_frequencies.get(new_pair, 0) + freq
+                            pair_frequencies[(token_tuple[i - 1], a)] = pair_frequencies.get((token_tuple[i - 1], a), 0) - freq
+                        if i + 2 < len(token_tuple):
+                            new_pair = (a + b, token_tuple[i + 2])
+                            pair_frequencies[new_pair] = pair_frequencies.get(new_pair, 0) + freq
+                            pair_frequencies[(b, token_tuple[i + 2])] = pair_frequencies.get((b, token_tuple[i + 2]), 0) - freq
 
-                    i += 2
-                else:
-                    merged.append(token_tuple[i])
-                    i += 1
+                        i += 2
+                    else:
+                        merged.append(token_tuple[i])
+                        i += 1
+            else:
+                continue
             frequency_table[tuple(merged)] = frequency_table.pop(token_tuple)
-        pair_frequencies[best_pair] = 0
-        # remove pairs with zero or negative frequency
-        for pair in list(pair_frequencies.keys()):
-            if pair_frequencies[pair] <= 0:
-                del pair_frequencies[pair]
+        del pair_frequencies[best_pair]
 
 
     time2 = time.time()
@@ -97,14 +97,27 @@ def train_bpe(input_path: str, vocab_size: int, special_tokens: list[str]) -> Tu
 
 
 if __name__ == "__main__":
-    vocab, merges = train_bpe("sample_text.txt", 10000, ["<|endoftext|>"])
-   # save results
-#     with open("bpe_vocab.txt", "w", encoding="utf-8") as f:
-#        for idx in sorted(vocab.keys()):
-#            f.write(f"{idx}\t{vocab[idx].decode('utf-8', errors='ignore')}\n")
-#     with open("bpe_merges.txt", "w", encoding="utf-8") as f:
-#         for merge in merges:
-#             f.write(f"{merge[0].decode('utf-8', errors='ignore')} {merge[1].decode('utf-8', errors='ignore')}\n")
-# #    print(vocab)
-#    print(merges)
-    # vocab, merges = train_bpe("sample_text.txt", 300, ["<|endoftext|>"])
+    path = "/scratch/work/zhul2/code/assignment1-basics/data/"
+    save_path = "/scratch/work/zhul2/code/assignment1-basics/cs336_basics/"
+    file = "owt_train.txt"
+    time1 = time.time()
+    vocab, merges = train_bpe(path + file, 10000, ["<|endoftext|>"])
+    time2 = time.time()
+    print(f"Total time taken: {time2 - time1} seconds")
+
+    bytes_to_unicode = gpt2_bytes_to_unicode()
+    for id, token_bytes in vocab.items():
+        token_str = ''.join(bytes_to_unicode[b] for b in token_bytes)
+        vocab[id] = token_str
+    vocab = {v: k for k, v in vocab.items()}  # invert vocab to match expected format
+    for i in range(len(merges)):
+        a_bytes, b_bytes = merges[i]
+        a_str = ''.join(bytes_to_unicode[b] for b in a_bytes)
+        b_str = ''.join(bytes_to_unicode[b] for b in b_bytes)
+        merges[i] = (a_str, b_str)
+    
+    with open(f'{save_path}{file[:-4]}.json', 'w') as f:
+        json.dump(vocab, f)
+    with open(f'{save_path}{file[:-4]}_merges.txt', 'w', encoding='utf-8') as f:
+        for merge in merges:
+            f.write(f"{merge[0]} {merge[1]}\n")
