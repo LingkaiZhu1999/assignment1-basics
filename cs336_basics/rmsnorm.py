4dd81b4d9426b2136a15b744affd73e1fb3039dc
LingkaiZhu1999 <zhulingkai123@gmail.com>
2026-02-02 13:03:22 +0800

    add rmsnorm

---
+++ cs336_basics/rmsnorm.py
@@ -0,0 +1,17 @@
+import torch
+
+class RMSNorm(torch.nn.Module):
+    def __init__(self, d_model: int, eps: float = 1e-5, device=None, dtype=None):
+        super().__init__()
+        self.d_model = d_model
+        self.eps = eps
+        self.gain = torch.nn.Parameter(torch.nn.init.trunc_normal_(
+            torch.empty((d_model), 
+            device=device, dtype=dtype)))
+        
+    def forward(self, x: torch.Tensor) -> torch.Tensor:
+        in_dtype = x.dtype
+        x = x.to(torch.float32)
+        rms = torch.sqrt(1. / self.d_model * torch.sum(torch.square(x), dim=-1, keepdim=True) + self.eps)
+        rmsnorm = x / rms * self.gain
+        return rmsnorm.to(in_dtype)
