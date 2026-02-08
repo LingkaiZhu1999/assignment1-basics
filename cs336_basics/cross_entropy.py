f710a41aa0ecb1d1fd9a4f943de44cfc198b435e
LingkaiZhu1999 <zhulingkai123@gmail.com>
2026-02-08 21:20:31 +0800

    concat seq

--- cs336_basics/cross_entropy.py
+++ cs336_basics/cross_entropy.py
@@ -1,4 +1,5 @@
 import torch
+from einops import rearrange
 from cs336_basics.softmax import softmax
 
 def cross_entropy(logits, targets):
@@ -6,4 +7,7 @@ def cross_entropy(logits, targets):
     shifted_logits = logits - max_logits
     log_sum_exp = torch.logsumexp(shifted_logits, dim=-1, keepdim=True)
     log_probs = shifted_logits - log_sum_exp
-    return -log_probs[torch.arange(len(targets)), targets].mean()
+    if log_probs.dim() == 3 and targets.dim() == 2:
+        log_probs = rearrange(log_probs, "batch seq vocab -> (batch seq) vocab")
+        targets = rearrange(targets, "batch seq -> (batch seq)")
+    return -log_probs[torch.arange(targets.shape[0], device=targets.device), targets].mean()
