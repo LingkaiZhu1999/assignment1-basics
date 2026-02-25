a483426b52e9433ea88621366283dad12197d760
Zhu Lingkai <zhulingkai123@gmail.com>
2026-02-25 16:52:01 +0200

    update train and valid

--- cs336_basics/data_loading.py
+++ cs336_basics/data_loading.py
@@ -19,9 +19,29 @@ def data_loading(x: np.array, batch_size: int, context_length: int, device: str)
     return torch.tensor(inputs, dtype=torch.long, device=device), torch.tensor(targets, dtype=torch.long, device=device)
 
 
+def valid_data_loading(x: np.array, batch_size: int, context_length: int, device: str, index: int):
+    max_start_index = len(x) - context_length
+    start_idx0 = index * batch_size
+
+    if start_idx0 >= max_start_index:
+        raise IndexError("Validation batch index out of range")
+
+    effective_batch_size = min(batch_size, max_start_index - start_idx0)
+
+    # Create input and target arrays
+    inputs = np.zeros((effective_batch_size, context_length), dtype=x.dtype)
+    targets = np.zeros((effective_batch_size, context_length), dtype=x.dtype)
+    for i, start_idx in enumerate(range(start_idx0, start_idx0 + effective_batch_size)):
+        inputs[i] = x[start_idx : start_idx + context_length]
+        targets[i] = x[start_idx + 1 : start_idx + context_length + 1]
+    
+    return torch.tensor(inputs, dtype=torch.long, device=device), torch.tensor(targets, dtype=torch.long, device=device)
+ 
 if __name__ == "__main__":
-    x = torch.randn((1000, ))
-    inputs, targets = data_loading(x, 8, 10, "cuda:0")
+    x = np.random.randint(0, 1000,(1000, ))
+    # inputs, targets = data_loading(x, 8, 10, "cuda:0")
+    # print(inputs, targets)
+    inputs, targets = valid_data_loading(x, 8, 10, "cuda:0", 0)
     print(inputs, targets)
     
     
--- cs336_basics/train.py
+++ cs336_basics/train.py
@@ -5,26 +5,73 @@ sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "..")))
 
 from cs336_basics.transformer_lm import Transformer_LM
 from cs336_basics.adamw import AdamW
-from cs336_basics.data_loading import data_loading
+from cs336_basics.data_loading import data_loading, valid_data_loading
 from cs336_basics.cross_entropy import cross_entropy
+from cs336_basics.checkpointing import load_checkpoint, save_checkpoint
+from cs336_basics.learning_rate_schedule import learning_rate_schedule
 
 import numpy as np
 import wandb
+import torch
 
 def train(run, args):
-    data_path = os.path.join(os.path.dirname(__file__), "..", "data", "sample_text.npy")
-    data = np.load(data_path, mmap_mode="r")
+    train_data_path = os.path.join(os.path.dirname(__file__), "..", "data", "tinystories_train_tokenized.npy")
+    train_data = np.load(train_data_path, mmap_mode="r")
+    valid_data_path = os.path.join(os.path.dirname(__file__), "..", "data", "tinystories_valid_tokenized.npy")
+    valid_data = np.load(valid_data_path, mmap_mode="r")
     model = Transformer_LM(args.d_model, args.num_heads, args.d_ff, args.vocab_size, args.context_length, args.num_layers, rope_theta=args.theta).to(args.device)
-    model.train()
-    optimizer = AdamW(model.parameters(), args.lr)
-    for iter in range(args.iterations):
+    optimizer = AdamW(
+        model.parameters(),
+        lr=args.lr_max,
+        betas=args.betas,
+        eps=args.eps,
+        weight_decay=args.weight_decay,
+    )
+
+    iterations = args.total_tokens_processed // args.batch_size // args.context_length
+    for iter in range(iterations):
+        model.train()
+        current_lr = learning_rate_schedule(iter, args.lr_max, args.lr_min, args.iter_warmup, t_cos_anneal=args.iter_cos_annealing)
+        for group in optimizer.param_groups:
+            group["lr"] = current_lr
         optimizer.zero_grad()
-        inputs, targets = data_loading(data, args.batch_size, args.context_length, device=args.device)
+        inputs, targets = data_loading(train_data, args.batch_size, args.context_length, device=args.device)
         outputs = model(inputs)
         loss = cross_entropy(outputs, targets)
         loss.backward()
         optimizer.step()
-        run.log({"loss": loss})
+        run.log({"loss/train": loss.item()})
+        valid_loss = validate(valid_data, model, args)
+        run.log({"loss/valid": valid_loss})
+    save_checkpoint(model, optimizer, iterations, "final_model.pt")
+
+
+def validate(valid_data, model, args):
+    max_start_index = len(valid_data) - args.context_length
+    if max_start_index <= 0:
+        return float("nan")
+
+    iters = (max_start_index + args.batch_size - 1) // args.batch_size
+    model.eval()
+    total_loss = 0.0
+    total_sequences = 0
+    with torch.no_grad():
+        for i in range(iters):
+            inputs, targets = valid_data_loading(
+                valid_data, args.batch_size, args.context_length, args.device, i
+            )
+            outputs = model(inputs)
+            loss = cross_entropy(outputs, targets)
+            current_batch_size = inputs.shape[0]
+            total_loss += loss.item() * current_batch_size
+            total_sequences += current_batch_size
+    return total_loss / total_sequences
+
+
+
+
+
+
 
 
 
@@ -38,17 +85,25 @@ if __name__ == "__main__":
         epilog="N/A",
     )
 
-    parser.add_argument("--iterations", type=int, default=100, help="total iterations for training")
-    parser.add_argument("--lr", type=float, default=1e-3, help="learning rate")
+    # parser.add_argument("--iterations", type=int, default=1000, help="total iterations for training")
+    parser.add_argument("--total_tokens_processed", type=int, default=327680000, help="batch size * total step count * context length")
+    parser.add_argument("--iter_warmup", type=int, default=1, help="warm up iterations")
+    parser.add_argument("--lr_max", type=float, default=1e-3, help="learning rate max")
+    parser.add_argument("--lr_min", type=float, default=1e-5, help="learning rate min")
+    parser.add_argument("--iter_cos_annealing", type=int, default=20000, help="iteration for cosine annealing")
+    parser.add_argument("--betas", type=float, nargs=2, default=(0.9, 0.999), help="betas for AdamW")
+    parser.add_argument("--eps", type=float, default=1e-8, help="eps for AdamW")
+    parser.add_argument("--weight_decay", type=float, default=0.01, help="weight decay (l1/l2 norm coefficient)")
     parser.add_argument("--batch_size", type=int, default=64, help="batch size")
-    parser.add_argument("--context_length", type=int, default=1024, help="max sequence length")
-    parser.add_argument("--d_model", type=int, default=10, help="dimension of model")
-    parser.add_argument("--d_ff", type=int, default=40,)
+    parser.add_argument("--context_length", type=int, default=256, help="max sequence length")
+    parser.add_argument("--d_model", type=int, default=512, help="dimension of model")
+    parser.add_argument("--d_ff", type=int, default=1344,)
     parser.add_argument("--vocab_size", type=int, default=10000, help="10000 for tinystories, 30000 for owt")
-    parser.add_argument("--num_layers", type=int, default=1)
-    parser.add_argument("--num_heads", type=int, default=5)
+    parser.add_argument("--num_layers", type=int, default=4)
+    parser.add_argument("--num_heads", type=int, default=16)
     parser.add_argument("--theta", type=float, default=10000, help="theta for rope")
     parser.add_argument("--device", type=str, default="cuda:0")
+
     args = parser.parse_args()
     print(args)
     run = wandb.init(project=parser.prog)
