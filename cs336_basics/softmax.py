753912d60ff8e8c2af12242061767e90ff331bab
LingkaiZhu1999 <zhulingkai123@gmail.com>
2026-02-25 15:14:43 +0800

    add temperature scaling softmax

--- cs336_basics/softmax.py
+++ cs336_basics/softmax.py
@@ -3,3 +3,7 @@ import torch
 def softmax(x: torch.Tensor, dim: int) -> torch.Tensor:
     x = x - x.max(dim=dim, keepdim=True).values # numerical stability
     return torch.exp(x) / torch.sum(torch.exp(x), dim=dim, keepdim=True)
+
+def temperature_scaling_softmax(x: torch.Tensor, dim: int, temperature: float) -> torch.Tensor:
+    x = x - x.max(dim=dim, keepdim=True).values # numerical stability
+    return torch.exp(x / temperature) / torch.sum(torch.exp(x / temperature), dim=dim, keepdim=True)
